79436edc73e0391a66a8ae43350412a1b6f49dc1
Rishi Raj <rishiraj@Rishis-MacBook-Air.local>
2025-03-23 20:35:21 +0530

    added mongo, crud and server

--- App.js
+++ App.js
@@ -307,7 +307,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   taskItem: {
-    backgroundColor: 'blue',
+    backgroundColor: 'black',
     padding: 15,
     marginVertical: 8,
     borderRadius: 10,
