ed8d16bada95566ee72356e3e4d99aaf34d84814
Rishi Raj <rishiraj@Rishis-MacBook-Air.local>
2025-04-07 01:08:48 +0530

    Removed config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-export const URL = "https://taskrecoback.onrender.com"; 
-
-export const API_LINK = 'https://api.groq.com/openai/v1/chat/completions';
-
-export const API_KEY = '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'; 
